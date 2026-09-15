321cb99bb52f591c2f9c513006aa99fab0ce7023
Greta Yan <monica24685@gmail.com>
2026-09-15 15:55:47 +0800

    新增 useExistingPermissionSheet 函數以支持使用現有的權限表，並確保必要的工作表存在

--- backend/Setup.js
+++ backend/Setup.js
@@ -21,3 +21,33 @@ function setup() {
   Logger.log('ADMIN_CODES＝可管理權限表的代碼，逗號分隔');
   Logger.log('LINK_物管系統、LINK_會議室預約、LINK_補課查看、LINK_遊覽車＝各系統的實際連結網址');
 }
+
+function useExistingPermissionSheet() {
+  var sheetId = '1C9FVElBDIZUlbAa5_y3GyBP2dx6HPyUTNtawY-QPPuE';
+  var ss = SpreadsheetApp.openById(sheetId);
+
+  var permSheet = ss.getSheetByName('permissions');
+  if (!permSheet) {
+    var sheets = ss.getSheets();
+    if (sheets.length === 1 && sheets[0].getLastRow() === 0) {
+      permSheet = sheets[0];
+      permSheet.setName('permissions');
+    } else {
+      permSheet = ss.insertSheet('permissions');
+    }
+  }
+  if (permSheet.getLastRow() === 0) {
+    permSheet.appendRow(['代碼', '姓名', '物管系統', '會議室預約', '補課查看', '遊覽車']);
+  }
+
+  var logSheet = ss.getSheetByName('logins');
+  if (!logSheet) {
+    logSheet = ss.insertSheet('logins');
+  }
+  if (logSheet.getLastRow() === 0) {
+    logSheet.appendRow(['時間', '代碼', '姓名']);
+  }
+
+  PropertiesService.getScriptProperties().setProperty('PERMISSION_SHEET_ID', sheetId);
+  Logger.log('已改用指定的 Sheet：' + ss.getUrl());
+}
